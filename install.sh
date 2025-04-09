539b8b58ba0115235756a00c6d252c691f53cc42
ZzqMa <buttlaface@gmail.com>
2025-04-09 01:51:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ chmod +x ~/toadssuckless/assets/autostart/autostart.sh
 mkdir ~/.dwm && cp ~/toadssuckless/assests/autostart.sh ~/.dwm/autostart.sh
 cp ~/toadssuckless/assets/kitty/kitty.conf ~/.config/kitty/kitty.conf
 cp ~/toadssuckless/assets/kitty/nord.conf ~/.config/kitty/nord.conf
+cd ~/toadssuckless/assets/slstatus && sudo make clean install
 echo thanks for using toads
